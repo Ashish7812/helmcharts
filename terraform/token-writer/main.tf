9096edff362afc766a2170c8645d5ef1d18e6341
sahoa <ashishkumar.sahoo@in.pega.com>
2025-12-28 18:28:23 +0530

    updated to new folders.

--- terraform/token-writer/main.tf
+++ terraform/token-writer/main.tf
@@ -23,7 +23,7 @@ provider "kubernetes" {
   alias                  = "remote"
   host                   = data.aws_eks_cluster.target.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.target.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster.auth.target.token
+  token                  = data.aws_eks_cluster_auth.target.token
 }
 provider "kubernetes" {
   alias = "mgmt"
