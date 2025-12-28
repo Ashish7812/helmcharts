e42c265ebc895b8693679db4b491bcd9c1c56ce3
sahoa <ashishkumar.sahoo@in.pega.com>
2025-12-28 19:37:30 +0530

    updated

--- terraform/token-writer/main.tf
+++ terraform/token-writer/main.tf
@@ -10,42 +10,83 @@ terraform {
 }
 
 # --- Providers ---
-provider "aws" { region = var.region }
-data "aws_eks_cluster" "target" { name = var.eks_cluster_name }
-data "aws_eks_cluster_auth" "target" { name = var.eks_cluster_name }
+provider "aws" {
+  region = var.region
+}
+data "aws_eks_cluster" "target" {
+  name = var.eks_cluster_name
+}
+data "aws_eks_cluster_auth" "target" {
+  name = var.eks_cluster_name
+}
 provider "kubernetes" {
   alias                  = "remote"
   host                   = data.aws_eks_cluster.target.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.target.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.target.token
 }
-provider "kubernetes" { alias = "mgmt" }
+provider "kubernetes" {
+  alias = "mgmt"
+}
 
 # --- Delay ---
-resource "time_sleep" "wait_for_auth_propagation" { create_duration = "15s" }
+resource "time_sleep" "wait_for_auth_propagation" {
+  create_duration = "15s"
+}
 
-# --- Remote Cluster RBAC & Token Secret ---
+# --- Remote Cluster RBAC & Token Secret (SYNTAX CORRECTED) ---
 resource "kubernetes_service_account" "flux_remote_helm" {
   provider   = kubernetes.remote
-  metadata { name = "flux-remote-helm"; namespace = var.remote_target_namespace }
+  metadata {
+    name      = "flux-remote-helm"
+    namespace = var.remote_target_namespace
+  }
   depends_on = [time_sleep.wait_for_auth_propagation]
 }
+
 resource "kubernetes_cluster_role" "flux_remote_helm_role" {
   provider   = kubernetes.remote
-  metadata { name = "flux-remote-helm-role" }
-  rule { api_groups = ["*"]; resources = ["*"]; verbs = ["*"] }
-  rule { non_resource_urls = ["*"]; verbs = ["*"] }
+  metadata {
+    name = "flux-remote-helm-role"
+  }
+  rule {
+    api_groups = ["*"]
+    resources  = ["*"]
+    verbs      = ["*"]
+  }
+  rule {
+    non_resource_urls = ["*"]
+    verbs             = ["*"]
+  }
   depends_on = [time_sleep.wait_for_auth_propagation]
 }
+
 resource "kubernetes_cluster_role_binding" "flux_remote_helm_binding" {
   provider   = kubernetes.remote
-  metadata { name = "flux-remote-helm-binding" }
-  role_ref { api_group = "rbac.authorization.k8s.io"; kind = "ClusterRole"; name = kubernetes_cluster_role.flux_remote_helm_role.metadata[0].name }
-  subject { kind = "ServiceAccount"; name = kubernetes_service_account.flux_remote_helm.metadata[0].name; namespace = var.remote_target_namespace }
+  metadata {
+    name = "flux-remote-helm-binding"
+  }
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = kubernetes_cluster_role.flux_remote_helm_role.metadata[0].name
+  }
+  subject {
+    kind      = "ServiceAccount"
+    name      = kubernetes_service_account.flux_remote_helm.metadata[0].name
+    namespace = var.remote_target_namespace
+  }
 }
+
 resource "kubernetes_secret" "flux_remote_sa_token" {
-  provider                       = kubernetes.remote
-  metadata { name = "flux-remote-helm-token"; namespace = var.remote_target_namespace; annotations = { "kubernetes.io/service-account.name" = kubernetes_service_account.flux_remote_helm.metadata[0].name } }
+  provider = kubernetes.remote
+  metadata {
+    name      = "flux-remote-helm-token"
+    namespace = var.remote_target_namespace
+    annotations = {
+      "kubernetes.io.service-account.name" = kubernetes_service_account.flux_remote_helm.metadata[0].name
+    }
+  }
   type                           = "kubernetes.io/service-account-token"
   wait_for_service_account_token = true
   depends_on                     = [kubernetes_cluster_role_binding.flux_remote_helm_binding]
@@ -54,7 +95,11 @@ resource "kubernetes_secret" "flux_remote_sa_token" {
 # --- STAGE 2 FINAL SECRET ---
 resource "kubernetes_secret" "intermediate_raw_token" {
   provider = kubernetes.mgmt
-  metadata { name = "tf-remote-raw-token-secret"; namespace = var.publish_secret_namespace; labels = { "managed-by" = "terraform", "purpose" = "intermediate-token" } }
+  metadata {
+    name      = "tf-remote-raw-token-secret"
+    namespace = var.publish_secret_namespace
+    labels    = { "managed-by" = "terraform", "purpose" = "intermediate-token" }
+  }
   type = "Opaque"
   data = {
     "token_b64"              = kubernetes_secret.flux_remote_sa_token.data["token"]
